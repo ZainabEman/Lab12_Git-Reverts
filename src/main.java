dd7839c91c5821363fe9258703f538426e0fc094
imama-kainat <imamakainat9@gmail.com>
2024-11-15 22:28:37 +0500

    Updated bug

--- src/main.java
+++ src/main.java
@@ -1,11 +1,10 @@
-
 public class main {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
+    public static void main(String[] args) {
+        System.out.println("Welcome to the application!");
+        // greetUser(); // Removed this line to introduce a bug
+    }
 
-}
-
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
+    public static void greetUser() {
+        System.out.println("Hello, User!");
+    }
 }
