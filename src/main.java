588a040bf8786d5fce329fcbfba653fca89bbc8d
imama-kainat <imamakainat9@gmail.com>
2024-11-15 22:30:08 +0500

    Revert "Updated bug"
    This reverts commit dd7839c91c5821363fe9258703f538426e0fc094.
    
    :

--- src/main.java
+++ src/main.java
@@ -1,10 +1,11 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the application!");
-        // greetUser(); // Removed this line to introduce a bug
-    }
 
-    public static void greetUser() {
-        System.out.println("Hello, User!");
-    }
+public class main {
+public static void main(String[] args) {
+System.out.println("Welcome to the application!");
+
+}
+
+public static void greetUser() {
+System.out.println("Hello, User!");
+}
 }
