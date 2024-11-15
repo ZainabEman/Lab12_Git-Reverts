cb6b84acb13b6f7d228c1da3ca6fbb78a2dc53e1
imama-kainat <imamakainat9@gmail.com>
2024-11-15 23:08:15 +0500

    main me changes krdiea task 4

--- src/main.java
+++ src/main.java
@@ -1,11 +1,7 @@
 
 public class main {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
+	public static void main(String[] args) {
+		System.out.println("Main branch update.");
+		}
 
-}
-
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
 }
