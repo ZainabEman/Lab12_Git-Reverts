51c8bef51544dc9b59279cd0a4f44f13b3cfaa89
Zainab Eman <zainabeman976@gmail.com>
2024-11-15 09:43:35 +0500

    code

--- src/main.java
+++ src/main.java
@@ -1,4 +1,11 @@
 
 public class main {
+public static void main(String[] args) {
+System.out.println("Welcome to the application!");
+greetUser();
+}
 
+public static void greetUser() {
+System.out.println("Hello, User!");
+}
 }
